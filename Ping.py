63d3dc1417df6c24a32e74160e2f476f9b602e1d
Lotikys <egorp4700@mail.ru>
2026-04-25 18:05:11 +0500

    Update Ping.py
    создание начальной сцены

---
+++ Ping.py
@@ -0,0 +1,101 @@
+from pygame import *
+from random import randint
+# подгружаем отдельно функции для работы со шрифтом
+font.init()
+font1 = font.SysFont("Arial", 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+
+
+font2 = font.SysFont("Arial", 36)
+
+
+#фоновая музыка
+mixer.init()
+
+
+
+# нам нужны такие картинки:
+score = 0 # сбито кораблей
+goal = 10 # столько кораблей нужно сбить для победы
+lost = 0 # пропущено кораблей
+max_lost = 3 # проиграли, если пропустили столько
+ 
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    
+
+
+# класс спрайта-врага  
+
+ 
+# класс спрайта-пули  
+ 
+# Создаем окошко
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+
+ 
+
+ 
+
+ 
+# переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+# Основной цикл игры:
+run = True # флаг сбрасывается кнопкой закрытия окна
+while run:
+    # событие нажатия на кнопку Закрыть
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        # событие нажатия на пробел - спрайт стреляет
+        
+ 
+  # сама игра: действия спрайтов, проверка правил игры, перерисовка
+    if not finish:
+        # обновляем фон
+        window.fill((120,120,205))
+
+
+       
+
+
+       
+        display.update()
+    # цикл срабатывает каждую 0.05 секунд
+    time.delay(50)
+
